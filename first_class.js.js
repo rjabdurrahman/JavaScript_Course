fbb93ed4120eaeb06a39a34c66683097dbe19b25
Abdur Rahman <asadsoon114@gmail.com>
2020-01-16 12:36:20 +0600

    Added: Code of first Class

---
+++ first_class.js.js
@@ -0,0 +1,60 @@
+/* Use 'Ctrl + /' to Uncomment */
+/* Print */
+// console.log(3)
+// console.log('Abdur Rahman', 'Apel Sarkar', 'Siam Mahmud')
+// console.log(
+//     'Apel',
+//     'Rahman',
+//     'Siam',
+//     'Luxman'
+// )
+// console.table([
+//     'Apel',
+//     'Rahman',
+//     'Siam',
+//     'Luxman'
+// ]);
+
+// console.table({
+//     name: 'Abdur Rahman',
+//     age: 21, 
+//     deparment: 'SWE'
+// })
+// console.log({name: 'Abdur Rahamn', age: 21, department: 'SWE'})
+
+/* Install Code Runner VS Code Extension */
+/* Install QuokkaJS VS Code Extension */
+
+/* Use 'Ctrl + Shift + P' for Command Plette */
+
+// console.log('I am okey');
+// console.log(23, 254, 23)
+// console.log(Math.max(121, 9 , 11, 0, 4))
+// console.table(['A', 'B', 'C'])
+
+/* Data Types
+Number
+String
+Boolean
+Object */
+// console.log(typeof 3)
+// console.log(typeof (3 - 1))
+// console.log(typeof 'Siam')
+// console.log(typeof "Danny")
+// console.log(typeof 'a')
+// console.log(typeof true)
+// console.log(typeof (3 == 2))
+// console.log(typeof {a: 10})
+// console.log(typeof new Object())
+// console.log(typeof [1, 2, 3, 4, 5])
+
+// Variables
+// var a, b, c;
+// var data;
+// var s = 10;
+// var name = 'JS Course';
+// const max = 50;
+// let min = 1;
+// console.log(s, name, max, min)
+
+
