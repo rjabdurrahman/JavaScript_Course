c870836246ee6cefd0930eb355eb68089de245cf
Abdur Rahman <asadsoon114@gmail.com>
2020-01-31 01:12:16 +0600

    Added: class 7 code for next video

--- class8.js
+++ class8.js
@@ -1,56 +1,36 @@
-/* Conditional Statements */
-// if(condition)
-// if(true) console.log('I am true')
-// var a = 11;
-// if(a > 10) console.log('I am greater than 10')
-// if(a > 10) {
-//   console.log('Its greater than 10')
-//   console.log('I am true block')
-// }
-// if(conditon)...else
-// if(a < 10) {
-//   console.log('Its greater than 10')
-//   console.log('I am true block')
-// }
-// else console.log('I am less than 10')
-// if(condition)...else if(condition)...else
-// Or if else ladder
-// var b = 15
-// if(b > 200) console.log('B is greater than 200')
-// else if(b > 100) console.log('B is greater than 100')
-// else if(b > 80) console.log('B is greater than 80')
-// else if(b > 70) console.log('B is greater than 80')
-// else if(b > 60) console.log('B is greater than 80')
-// else console.log('B is less than 60')
-// if(condition) { if(conditon)...else } else
-// Or nested if else
-// var m = 1;
-// var n = -2;
-// if(m > 0) {
-//   if(n > 0) console.log('Both are positive')
-//   else console.log('M is positive but N is negative')
-// }
-// else {
-//   if(n > 0) console.log('M is negative but N is positive')
-//   else console.log('Both are negative')
-// }
-// switch(value)
-// var p = 1
-// switch(p) {
-//   case 1: console.log('P is 1')
-//   break;
-//   case 2: console.log('P is 2')
-//   break;
-//   case 3: console.log('P is 3')
-//   break;
-//   case 4: console.log('P is 4')
-//   break;
-//   case 5: console.log('P is 5')
-//   break;
-//   default: console.log('P is none')
-// }
-
-/* Conditional Operators */
-// condition ? True Statement : False Statement
-var n = 10
-n < 9 ? console.log('N is greater than 9') : console.log('N is smaller than 9')
+/* Loops */
+// for
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+  }
+  console.log(i)
+  // for...in
+  var arr = [12, 14, 5, 15, 75, 12]
+  for (i in arr) {
+    console.log(arr[i])
+  }
+  console.log(i)
+  // for...of
+  for (i of arr) {
+    console.log(i)
+  }
+  console.log(i)
+  // while
+  var j = 0
+  while (j < 11) {
+    console.log(j)
+    j++
+  }
+  console.log(j)
+  // do...while
+  do {
+    console.log(j)
+    j++
+  } while (j < 17)
+  // forEach
+  
+  var students = ['Abir', 'Siam', 'Simanto', 'Kobir']
+  students.forEach(function (x) {
+    console.log(x)
+  })
+  
