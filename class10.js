aa8251b14cf044f4f4a92978cf2f4bc2d1a66f57
Abdur Rahman <asadsoon114@gmail.com>
2020-02-06 12:00:36 +0600

    Added: Class10 String

--- class10.js
+++ class10.js
@@ -1,6 +1,14 @@
-/* Scope */
-/* var, let, const */
+/* JavaScript String */
+var a = 'Hunny Coders'
+var b = 'Learn Something New'
+console.log(a, b)
+console.log(a + ' ' + b)
+var line = `We are the Hunny Coders team and 
+            we want to do something new`
+console.log(line)
 
-// Local Scope and Variable
-// Global Scope Variable
-// Block Scope Variable
+var name = 'Abdur Rahman'
+var title = 'Computer Programmer'
+console.log('Hello ' + name)
+console.log('Mr. ' + name + ', he is a ' + title)
+console.log(`Mr. ${name}, he is a ${title}`)
