01d968eed46e86447e5e2e01f301872c71e39be9
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-07-31 14:10:14 +0600

    Updated: Class 15 and 16 for Array Functions

--- class14.js
+++ class14.js
@@ -1,9 +1,65 @@
-var array = new Array('Siam', 'Robiul', 'Dilam', 'Farhad')
-// console.log(array)
-// console.log(array[0])
-// console.log(array[1])
-// console.log(array[2])
-// console.log(array[3])
-// console.log(array[4])
-for(i = 0; i < 4; i++) console.log(i, array[i])
-console.table(array)
+// Why Array
+var friend1 = 'Farhad Hossen'
+var friend2 = 'Siam'
+var friend3 = 'Fardeen Rahman'
+var friend4 = 'Jubaier Rahman'
+var friend5 = 'Sakib Al Islam'
+var friend6 = 'Shakif'
+// Array Declartion
+// var friends = [
+//     'Farhad Hossen',
+//     'Siam',
+//     'Fardeen',
+//     'Jubaier',
+//     'Sakib',
+//     'Shakif'
+// ];
+// console.log(friends)
+// Array Access
+// console.log(friends[0])
+// console.log(friends[1])
+// console.log(friends[2])
+// console.log(friends[3])
+// console.log(friends[4])
+// Array Index
+// Array Elemnts
+// console.log(friends.length)
+// for(i = 0; i < friends.length; i++) {
+//     console.log(i, friends[i])
+// }
+// Access Arrlay Elents with For Loops
+// for(i in friends) console.log(friends[i])
+// for(i of friends) console.log(i)
+// Object + Array
+// var student1 = {
+//     roll: 1,
+//     firstName: 'Sakib',
+//     lastName: 'Sultan'
+// }
+var students = [
+    {
+        roll: 1,
+        firstName: 'Sakib',
+        lastName: 'Sultan',
+        subjects: ['English', 'Bangla', 'Match', 'Science']
+    },
+    {
+        roll: 2,
+        firstName: 'Farhad',
+        lastName: 'Hossen'
+    },
+    {
+        roll: 3,
+        firstName: 'Hasan',
+        lastName: 'Sharif'
+    },
+    {
+        roll: 4,
+        firstName: 'Mohidul',
+        lastName: 'Islam'
+    }
+]
+console.log(students[0].subjects[0])
+// for(student of students) {
+//     console.log(student.roll, student.firstName)
+// }
---
+++ class16.js
@@ -0,0 +1,17 @@
+// Part 2
+// forEach
+// numbers.forEach(function(x, i, arr) {
+//     console.log(i, x)
+// })
+// map
+// let newNums = numbers.map(function(x, i, arr) {
+//     return x * 2;
+// })
+// let stNames = students.map(x => x.age)
+// console.log(stNames)
+// find
+// findIndex
+// sort
+// reduce
+// some
+// every
